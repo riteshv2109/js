69e2d9bf783d999979cb38a3e4ac793a5272d710
riteshv2109 <riteshv2109@gmail.com>
2025-01-05 07:49:34 +0000

    conversion of datatypes

---
+++ basics/conversionoperations.js
@@ -0,0 +1,18 @@
+let score=true
+console.log(typeof score);
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33" => 33 easily convertible
+// 33ab => NaN (not a number)
+// true => 1; false => 0
+// 1 => true ; 0 => false;
+// " " => false ; " abc" => true
+
+let somenumber=33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber)
