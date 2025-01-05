904f10e32ebca071eee5b907750c28872c0cae26
riteshv2109 <riteshv2109@gmail.com>
2025-01-05 11:43:47 +0000

    data types summary

---
+++ basics/datatypessummary.js
@@ -0,0 +1,26 @@
+// primitive (call by value function)
+
+// 7 types : null;undefined;string;number;bigint;symbol;boolean
+
+// reference (non primitive)(call by reference funtion)
+
+// arrays;objects;functions
+
+const id=Symbol("123")
+console.log(id);
+
+const heroes = ["tony","thor"]
+
+// ab neeche wale object hai
+{
+//   curly braces  k ander jitna bhi kuch hai sab object hai
+     name : "ritesh"
+     age : 20
+}
+
+const myFunction = function()
+{
+    console.log("hello world ");
+    
+}
+console.log(myFunction);
