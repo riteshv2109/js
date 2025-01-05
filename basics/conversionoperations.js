d0b4ef6452d583338b9e7ec7e61cd54e9a2abe6f
riteshv2109 <riteshv2109@gmail.com>
2025-01-05 10:13:59 +0000

    operations

--- basics/conversionoperations.js
+++ basics/conversionoperations.js
@@ -1,9 +1,9 @@
 let score=true
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueinnumber=Number(score)
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
 
 // "33" => 33 easily convertible
 // 33ab => NaN (not a number)
@@ -14,5 +14,39 @@ console.log(valueinnumber);
 let somenumber=33
 
 let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber)
+
+let value = 3
+let negvalue=-value
+console.log(negvalue);
+
+let str1 = "ritesh"
+let str2 = " verma"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+//  output is true
+
+console.log(+true);
+// output is 1
+
+// console.log(true+);  not possible
+
+let gamecounter= 100
+gamecounter++
+
+console.log(gamecounter);
+
+
+
+
+
+
